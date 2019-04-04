3cb8f9cceb7e824667bcbe006169f0076b66afe0
Oskari Pirttikoski <oskari.pirttikoski@reaktor.fi>
2019-04-04 21:49:12 +0300

    use recently added control distance in XML
    Distance is there only from year 2014.

--- README.md
+++ README.md
@@ -9,14 +9,14 @@ pipenv sync
 Fetch xml files (history) and running order for year to predict:
 
 ```bash
-time for year in $(seq 2011 2017); do echo "YEAR $year"; time wget -P data http://results.jukola.com/tulokset/results_j${year}_ju.xml; done
+time for year in $(seq 1992 2018); do echo "YEAR $year"; time wget -P data https://results.jukola.com/tulokset/results_j${year}_ju.xml; done
 time pipenv run python fetch_running_order.py 2017 && wc data/running_order_j2017_ju.tsv
 ```
 
 Convert xml to csv and join years by runner name and team:
 
 ```bash
-time for year in $(seq 2012 2017); do echo "YEAR $year"; time pipenv run python result_xml_to_csv.py $year ve && head data/csv-results_j${year}_ve.tsv; done
+time for year in $(seq 1992 2018); do echo "YEAR $year"; time pipenv run python result_xml_to_csv.py $year ve && head data/results_with_dist_j${year}_ve.tsv; done
 time pipenv run python group_csv.py ve && head data/grouped_paces_ve.tsv
 ```
 
--- result_xml_to_csv.py
+++ result_xml_to_csv.py
@@ -2,43 +2,63 @@ import csv
 import datetime
 import logging
 import sys
+import numpy as np
+import math
 import time
 import xml.etree.ElementTree as ET
 
 # time pipenv run python result_xml_to_csv.py 2017 ve
 
-# time for year in $(seq 2012 2018); do echo "YEAR $year"; time pipenv run python result_xml_to_csv.py $year ve && head data/csv-results_j${year}_ve.tsv; done
+# time for year in $(seq 2012 2018); do echo "YEAR $year"; time pipenv run python result_xml_to_csv.py $year ve && head data/results_with_dist_j${year}_ve.tsv; done
 
 # wget -P data http://online.jukola.com/tulokset/results_j2018_ju.xml
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
 year = sys.argv[1]
 ve_or_ju = sys.argv[2]
-tree = ET.parse(f'data/results_j{year}_{ve_or_ju}.xml')
+input_file = f'data/results_j{year}_{ve_or_ju}.xml'
+logging.info("Reading " + input_file)
+tree = ET.parse(input_file)
 root = tree.getroot()
 
 # open a file for writing
 
-out_file_name = f'data/csv-results_j{year}_{ve_or_ju}.tsv'
+out_file_name = f'data/results_with_dist_j{year}_{ve_or_ju}.tsv'
 csv_file = open(out_file_name, 'w')
 
 # create the csv writer object
 
 csvwriter = csv.writer(csv_file, delimiter="\t", quoting=csv.QUOTE_ALL)
 header = ["team-id", "placement", "team-time", "team-name", "team-nro", "leg-nro", "emit", "leg-time",
-          "competitor-name", "control-times"]
+          "competitor-name", "weighted_avg_pace", "weighted_pace_std", "disqualified"]
 
 csvwriter.writerow(header)
 
 
 def parse_time(control_time_text):
     try:
-        return time.strptime(control_time_text, '%M:%S')
+        return time.strptime(control_time_text, '%S')
     except ValueError:
         try:
-            return time.strptime(control_time_text, '%H:%M:%S')
+            return time.strptime(control_time_text, '%M:%S')
         except ValueError:
-            print("Cannot parse " + control_time_text)
-            raise
+            try:
+                return time.strptime(control_time_text, '%H:%M:%S')
+            except ValueError:
+                print("Cannot parse " + control_time_text)
+                raise
+
+def weighted_avg_and_std(values, weights):
+    """
+    Return the weighted average and standard deviation.
+
+    values, weights -- Numpy ndarrays with the same shape.
+    """
+    average = np.average(values, weights=weights)
+    # Fast and numerically precise:
+    variance = np.average((values-average)**2, weights=weights)
+    return (average, math.sqrt(variance))
 
 
 for team in root.iter('team'):
@@ -53,28 +73,45 @@ for team in root.iter('team'):
         row.append(leg.find("legnro").text)
         row.append(leg.findtext("emit", "NA"))
 
-        control_times = []
-        for control_time in leg.iter('ct'):
-            struct_time = parse_time(control_time.text)
-            ct_secs = datetime.timedelta(hours=struct_time.tm_hour, minutes=struct_time.tm_min,
-                                         seconds=struct_time.tm_sec).total_seconds()
-            control_times.append(str(int(ct_secs)))
-
         # if no tsecs (=runner or team disqualified?)
         default_leg_time = "NA"
         row.append(leg.findtext("tsecs", default_leg_time))
 
-        row.append(leg.find("nm").text)
+        competitor_name = leg.find("nm").text
+        row.append(competitor_name)
 
-        control_times = []
-        for control_time in leg.iter('ct'):
-            struct_time = parse_time(control_time.text)
-            ct_secs = datetime.timedelta(hours=struct_time.tm_hour, minutes=struct_time.tm_min,
-                                         seconds=struct_time.tm_sec).total_seconds()
-            control_times.append(str(int(ct_secs)))
+        control_paces = []
+        control_distances = []
+        disqualified = False
+        for control in leg.iter('control'):
+            cd_text = control.find("cd").text
+            if cd_text == "-" or cd_text is None:
+                disqualified = True # Top teams have these but are not disqualified
 
-        row.append(";".join(control_times))
+            else:
+                struct_time = parse_time(cd_text)
+                cd_secs = int(datetime.timedelta(hours=struct_time.tm_hour, minutes=struct_time.tm_min,
+                                                 seconds=struct_time.tm_sec).total_seconds())
+                distance_element = control.find("cl")
+                if distance_element is not None:
+                    distance_meters = int(distance_element.text)
+                    control_pace = (cd_secs / 60.0) / (distance_meters / 1000.0)
 
+                    control_paces.append(control_pace)
+                    control_distances.append(distance_meters)
+
+        if len(control_paces) > 0:
+            # TODO use log values
+            (weighted_avg_pace, weighted_pace_std) = weighted_avg_and_std(control_paces, control_distances)
+            #logging.info(f"{weighted_avg_pace} {weighted_pace_std}")
+        else:
+            (weighted_avg_pace, weighted_pace_std) = ("NA", "NA")
+            #logging.info(control_paces)
+            #logging.info(control_distances)
+            #logging.info(f"{competitor_name}: {weighted_avg_pace} {weighted_pace_std}")
+        row.append(weighted_avg_pace)
+        row.append(weighted_pace_std)
+        row.append(disqualified)
         csvwriter.writerow(row)
 
 csv_file.close()
