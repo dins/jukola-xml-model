f0ed45999317b8d140c7e78296f1aad62a620cfd
Oskari Pirttikoski <oskari.pirttikoski@reaktor.fi>
2019-06-01 11:42:34 +0300

    latest experiments

--- shared.py
+++ shared.py
@@ -24,12 +24,58 @@ def preprocess_features(runs_df, top_countries, top_first_names):
 
     # Explode categories to dummy columns
     features = pd.get_dummies(runs[["leg", "c", "runs", "fn"]], sparse=True)
+
     # allow linear regression to fit non-linear terms
     # features["team_id_log2"] = np.log2(runs.team_id)
-    features["team_id_log10"] = np.log10(runs.team_id)
-    features["team_id_log100"] = np.log(runs.team_id) / np.log(100)
-    features["team_id_square"] = np.square(runs.team_id)
+    #features["team_id_log10"] = np.log10(runs.team_id)
+    #features["team_id_log100"] = np.log(runs.team_id) / np.log(100)
+    #features["team_id_square"] = np.square(runs.team_id)
 
+    #features.insert(0, "team_id_square", np.square(runs.team_id))
+    features.insert(0, "team_id_log10", np.log10(runs.team_id))
     features.insert(0, "team_id", runs["team_id"])
 
     return features
+
+
+def preprocess_and_make_np_pd_frames(runs_df, list_of_features):
+
+    def truncate_to_top_values(value, top_values):
+        if value in top_values:
+            return value
+        else:
+            return "OTHER"
+
+    features = runs_df.assign(leg=runs_df.leg_nro.astype(str))
+    #truncate runs
+    features["runs"] = np.clip(features.num_runs, 0, 8 )
+    #first name split
+    features["first_name"] = features.name.str.split(" ", expand=True).iloc[:, 0]
+
+    #set country to one of the top countries or other
+    country_counts = runs_df["team_country"].value_counts()
+    top_country_counts = country_counts[country_counts > 100]
+    top_countries = top_country_counts.keys().tolist()
+    features["c"] = features.apply(lambda run: truncate_to_top_values(run["team_country"], top_countries), axis=1)
+
+    #hierarchical group from full name
+    labels, uniques = pd.factorize(features.name.values)
+
+    #ID transforms
+    features["team_id_log10"] = np.log10(features.team_id)
+    features["team_id_log100"] = np.log(features.team_id) / np.log(100)
+    features["team_id_square"] = np.square(features.team_id)
+
+
+    #make df with names from Venlat
+    ve_data = pd.read_csv(f'data/runs_ve.tsv', delimiter="\t")
+    ve_data["first_name"] = ve_data.name.str.split(" ", expand=True).iloc[:, 0]
+    features["ve_name"]=runs_df.first_name.isin(set(ve_data.first_name))
+    features['ve_name']=features['ve_name']*1
+
+    #select only the named features
+    features = features[list_of_features]
+    #explode leg and country
+    numpy_frame_x = pd.get_dummies(data=features, columns=["leg","c"], drop_first=True, sparse=True).drop(columns=["pace"]).values
+    numpy_y = features["pace"].values
+    return numpy_frame_x, numpy_y, features, labels
