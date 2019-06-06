50b7ff16dc249cecfc1e364349e207859748e60c
Oskari Pirttikoski <oskari.pirttikoski@reaktor.fi>
2019-06-06 06:10:00 +0300

    cluster first names based on pace and pace deviation
    400 first name features becomes 14 with clustering.

--- shared.py
+++ shared.py
@@ -63,12 +63,11 @@ def predict_without_history(features):
     return gbr_sd_estimate
 
 
-def preprocess_features(runs_df, top_countries, top_first_names):
+def preprocess_features(runs_df, top_countries, ve_or_ju):
     display(runs_df.info())
     # convert some int columns to labels
     runs = runs_df.assign(leg=runs_df.leg_nro.astype(str))
     runs["runs"] = np.clip(runs.num_runs, 0, 8).astype(str)
-    runs = runs.drop(["leg_nro", "year", "team"], axis=1)
 
     def truncate_to_top_values(value, top_values):
         if value in top_values:
@@ -76,15 +75,19 @@ def preprocess_features(runs_df, top_countries, top_first_names):
         else:
             return "OTHER"
 
-    # A columns that contains most poular first names
+    # First name based pace category
     runs["first_name"] = runs.name.str.split(" ", expand=True).iloc[:, 0]
-    runs["fn"] = runs.apply(lambda run: truncate_to_top_values(run["first_name"], top_first_names), axis=1)
+    name_pace_classes = pd.read_csv(f"data/name_pace_classes_{ve_or_ju}.tsv", delimiter="\t")
+    display(name_pace_classes.info())
+    runs = runs.join(name_pace_classes.set_index('first_name'), on="first_name")
+    runs["fn_pace_class"] = runs["fn_pace_class"].astype(str)
+    runs["fn_pace_std_class"] = runs["fn_pace_std_class"].astype(str)
 
     # Add column for most popular countries
     runs["c"] = runs.apply(lambda run: truncate_to_top_values(run["team_country"], top_countries), axis=1)
 
     # Explode categories to dummy columns
-    features = pd.get_dummies(runs[["leg", "c", "runs", "fn"]], sparse=True)
+    features = pd.get_dummies(runs[["leg", "c", "runs", "fn_pace_class", "fn_pace_std_class"]], sparse=True)
 
     # allow linear regression to fit non-linear terms
     # features["team_id_log2"] = np.log2(runs.team_id)
