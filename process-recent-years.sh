22cf3247f8ef616b098836438282066e575f3b8e
Oskari Pirttikoski <oskari.pirttikoski@reaktor.fi>
2021-09-30 09:31:23 +0300

    reduce unnecessary decimals from analysis report

--- process-recent-years.sh
+++ process-recent-years.sh
@@ -9,3 +9,5 @@ RACE_TYPE=ve FORECAST_YEAR=2019 time ./process-one-race.sh
 RACE_TYPE=ju FORECAST_YEAR=2019 time ./process-one-race.sh
 RACE_TYPE=ve FORECAST_YEAR=2021 time ./process-one-race.sh
 RACE_TYPE=ju FORECAST_YEAR=2021 time ./process-one-race.sh
+
+git diff --color=always --word-diff=color -U0 reports/ | grep -E "aikaväliennuste väärin|keskivirhe"
