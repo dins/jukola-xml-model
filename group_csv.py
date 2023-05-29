a9f5f41a0dd24c1759e18da8af4721aa224fd524
Oskari Pirttikoski <oskari.pirttikoski@reaktor.fi>
2023-05-29 18:29:28 +0300

    improve runner grouping by several tricks
    * connect teams using emit numbers
    * allow repeating runs in same team
    * allow ONE run in another team without separating that run to a new indidual

--- group_csv.py
+++ group_csv.py
@@ -4,18 +4,19 @@ import logging
 from collections import defaultdict
 
 import numpy as np
+import pandas as pd
 
 import normalize_names
 import shared
 
 # RACE_TYPE=ve FORECAST_YEAR=2022 time poetry run python group_csv.py && head data/grouped_paces_ve.tsv
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s [%(process)d] %(funcName)s [%(levelname)s] %(message)s')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s [%(process)d] %(funcName)s [%(levelname)s] %(message)s')
 
 ve_or_ju = shared.race_type()
 
 by_name = defaultdict(list)
-
 
 for year in shared.history_years():
     country_by_team_id = shared.read_team_countries(year, ve_or_ju)
@@ -68,20 +69,83 @@ def open_output_file(out_file_name, column_names):
 
 
 by_unique_name = {}
+
+
+def _connect_teams_by_emit(runs):
+    runs_by_team = defaultdict(list)
+    for run in runs:
+        runs_by_team[run["team"]].append(run)
+    runs_by_emit = defaultdict(list)
+    for run in runs:
+        if "NA" not in run["emit"]:
+            runs_by_emit[run["emit"]].append(run)
+    # over years emit can connect teams
+    teams_connected_by_emit = defaultdict(set)
+    for emit, emit_runs in runs_by_emit.items():
+        for emit_run in emit_runs:
+            unique_teams = set(emit_run["team"] for emit_run in emit_runs)
+            unique_teams.remove(emit_run["team"])
+            teams_connected_by_emit[emit_run["team"]].update(unique_teams)
+
+    def _get_connected_teams(team, found_connections):
+        found_connections.add(team)
+        for connection in teams_connected_by_emit[team]:
+            if connection not in found_connections:
+                found_connections.update(_get_connected_teams(connection))
+        return found_connections
+
+    connected_teams_sets = set()
+    for team, connected_teams in teams_connected_by_emit.items():
+        connected_teams_sets.add(frozenset(_get_connected_teams(team, connected_teams)))
+
+    connected_teams_sets = [connections for connections in connected_teams_sets if len(connections) > 1]
+
+    return connected_teams_sets
+
+
 for name, runs in by_name.items():
     run_years = list(map(lambda run: run["year"], runs))
     unique_years = set(run_years)
 
-    if len(run_years) == len(unique_years):
+    qualified_runs = [run for run in runs if run["pace"] != "NA"]
+
+    # try to distinguish different people with same name from each other
+    # kaima vs tuplaaja
+    # namesakes vs those individuals who run multiple legs in one race
+    unique_teams_by_year = defaultdict(set)
+    for run in qualified_runs:
+        year = run["year"]
+        unique_teams_by_year[year].add(run["team"])
+
+    years_with_multiple_teams = [year for year, teams in unique_teams_by_year.items() if len(teams) > 1]
+    has_multiple_teams_at_least_in_one_year = len(years_with_multiple_teams) > 0
+    has_multiple_teams_only_in_one_year = len(years_with_multiple_teams) == 1
+
+    connected_teams_sets = _connect_teams_by_emit(runs)
+
+    if len(years_with_multiple_teams) > 1:
+        logging.info(
+            f"{name} Multiple teams, ONE: {has_multiple_teams_only_in_one_year} {years_with_multiple_teams=} {connected_teams_sets=}\n{pd.DataFrame.from_dict(runs)}")
+
+    if not has_multiple_teams_at_least_in_one_year or has_multiple_teams_only_in_one_year:
         by_unique_name[name] = runs
     else:
-        by_team = defaultdict(list)
+        by_team_set = defaultdict(list)
         for run in runs:
             team_name = run["team"]
-            by_team[team_name].append(run)
-        for team_name, runs_in_team in by_team.items():
-            unique_name = name + ":" + team_name
-            by_unique_name[unique_name] = runs_in_team
+            team_sets = [connected_teams_set for connected_teams_set in connected_teams_sets if
+                         team_name in connected_teams_set]
+            if len(team_sets) == 0:
+                key_set = frozenset([team_name])
+            else:
+                key_set = frozenset(team_sets[0])
+            by_team_set[key_set].append(run)
+        for team_set, runs_in_teams in by_team_set.items():
+            # logging.info(f"{name=}, {team_set=}, {runs_in_teams=}")
+            team_names = ';'.join(team_set)
+            unique_name = f"{name}:{team_names}"
+            by_unique_name[unique_name] = runs_in_teams
+            logging.info(f"{unique_name} SPLIT\n{pd.DataFrame.from_dict(runs_in_teams)}")
 
 column_names = ["mean_team_id", "teams", "name", "num_runs", "num_valid_times", "mean_pace", "stdev", "log_stdev",
                 "most_common_leg", "most_common_country", "years_and_legs"] + shared.pace_columns
@@ -136,7 +200,8 @@ for unique_name, runs in by_unique_name.items():
 out_file.close()
 
 # TODO add "leg_distance" column to runs.tsv
-runs_file_cols = ["name", "year", "team_id", "team", "team_country", "pace", "leg", "num_runs", "median_pace", "log_stdev"]
+runs_file_cols = ["name", "year", "team_id", "team", "team_country", "pace", "leg", "num_runs", "median_pace",
+                  "log_stdev"]
 (runs_out_file, runs_csvwriter) = open_output_file(f'data/runs_{shared.race_id_str()}.tsv',
                                                    runs_file_cols)
 
