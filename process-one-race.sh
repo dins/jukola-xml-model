dc4fd9355d14228df2495b9d7e190f7772bd3009
Oskari Pirttikoski <oskari.pirttikoski@reaktor.fi>
2024-06-06 22:36:03 +0300

    changes because of pymc model

--- process-one-race.sh
+++ process-one-race.sh
@@ -22,8 +22,11 @@ echo $(date -u +"%F %T") "personal_coefficients ${RACE_TYPE} ${FORECAST_YEAR} DO
 time poetry run python prepare_run_features.py
 echo $(date -u +"%F %T") "prepare_run_features.py ${RACE_TYPE} ${FORECAST_YEAR} DONE"
 
-time poetry run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=1200 --execute 2019-relay-simulation.ipynb
-echo $(date -u +"%F %T") "2019-relay-simulation ${RACE_TYPE} ${FORECAST_YEAR} DONE"
+#time poetry run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=1200 --execute 2019-relay-simulation.ipynb
+#echo $(date -u +"%F %T") "2019-relay-simulation ${RACE_TYPE} ${FORECAST_YEAR} DONE"
+
+time poetry run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=1200 --execute relay-simulation-2024.ipynb
+echo $(date -u +"%F %T") "relay-simulation-2024.ipynb ${RACE_TYPE} ${FORECAST_YEAR} DONE"
 
 if [[ -z "${BEFORE_RACE}"  ]]; then
   time poetry run jupyter nbconvert --to notebook --inplace --execute post-race-analysis.ipynb
--- process-recent-years.sh
+++ process-recent-years.sh
@@ -13,31 +13,29 @@ function process_one_race {
   LOG_PATH="logs/parallel-${RACE_TYPE}-${FORECAST_YEAR}-${RUN_TS}.log"
   start_secs=$SECONDS
   echo $(date -u +"%F %T") "Starting at ${start_secs} secs, ${LOG_PATH}"
-  RUN_TS=${RUN_TS} ./process-one-race.sh &> ${LOG_PATH} || echo $(date -u +"%F %T") "FAILED ${LOG_PATH}"
+  RUN_TS=${RUN_TS} ./process-one-race.sh &>${LOG_PATH} || echo $(date -u +"%F %T") "FAILED ${LOG_PATH}"
   duration=$((SECONDS - start_secs))
   echo $(date -u +"%F %T") "DONE ${LOG_PATH} in $duration secs"
 }
 
 RACE_TYPE=ju FORECAST_YEAR=2023 process_one_race &
 RACE_TYPE=ve FORECAST_YEAR=2023 process_one_race &
-RACE_TYPE=ju FORECAST_YEAR=2017 process_one_race &
+RACE_TYPE=ju FORECAST_YEAR=2022 process_one_race &
+RACE_TYPE=ve FORECAST_YEAR=2022 process_one_race &
 sleep 2
-RACE_TYPE=ve FORECAST_YEAR=2017 process_one_race &
-sleep 2
-RACE_TYPE=ju FORECAST_YEAR=2018 process_one_race &
-sleep 2
-RACE_TYPE=ve FORECAST_YEAR=2018 process_one_race &
+RACE_TYPE=ju FORECAST_YEAR=2021 process_one_race &
+RACE_TYPE=ve FORECAST_YEAR=2021 process_one_race &
 sleep 2
 RACE_TYPE=ju FORECAST_YEAR=2019 process_one_race &
-sleep 2
 RACE_TYPE=ve FORECAST_YEAR=2019 process_one_race &
 
 wait
 
-RACE_TYPE=ju FORECAST_YEAR=2021 process_one_race &
-RACE_TYPE=ve FORECAST_YEAR=2021 process_one_race &
-RACE_TYPE=ju FORECAST_YEAR=2022 process_one_race &
-RACE_TYPE=ve FORECAST_YEAR=2022 process_one_race &
+RACE_TYPE=ju FORECAST_YEAR=2018 process_one_race &
+RACE_TYPE=ve FORECAST_YEAR=2018 process_one_race &
+sleep 2
+RACE_TYPE=ju FORECAST_YEAR=2017 process_one_race &
+RACE_TYPE=ve FORECAST_YEAR=2017 process_one_race &
 
 wait
 
@@ -47,4 +45,3 @@ BEFORE_RACE="true" RACE_TYPE=ju FORECAST_YEAR=2024 process_one_race
 echo "DONE ${RUN_TS}"
 
 time poetry run python json_reports.py
-
