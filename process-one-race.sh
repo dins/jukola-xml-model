fa8300f6c4653f8d27912127e68765d16595a0e5
Oskari Pirttikoski <oskari.pirttikoski@reaktor.fi>
2026-03-15 19:36:18 +0200

    rerun 2025

--- process-one-race.sh
+++ process-one-race.sh
@@ -13,16 +13,11 @@ echo $(date -u +"%F %T") "Starting ngboost  ${RACE_TYPE} ${FORECAST_YEAR} in ano
 # Run in subshell
 (cd ~/koodi/jukola-ngboost; time RACE_TYPE=${RACE_TYPE} FORECAST_YEAR=${FORECAST_YEAR} BATCH_RUN_TS=${RUN_TS} PROCESSING_BATCH_ID=ngboost-student-t uv run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=36000  --execute ngboost-norm-tuned.ipynb)
 #(cd ~/koodi/jukola-ngboost; time RACE_TYPE=${RACE_TYPE} FORECAST_YEAR=${FORECAST_YEAR} PROCESSING_BATCH_ID=ngboost-student-t uv run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=36000  --execute ngboost-skwenorm.ipynb)
-#(cd ~/koodi/jukola-ngboost; time RACE_TYPE=${RACE_TYPE} FORECAST_YEAR=${FORECAST_YEAR} PROCESSING_BATCH_ID=ngboost-student-t uv run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=36000  --execute ngboost-student-t.ipynb)
-#(cd ~/koodi/jukola-ngboost; time RACE_TYPE=${RACE_TYPE} FORECAST_YEAR=${FORECAST_YEAR} PROCESSING_BATCH_ID=ngboost-student-t uv run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=36000  --execute ngboost-student-t-weights.ipynb)
 echo $(date -u +"%F %T") "ngboost ${RACE_TYPE} ${FORECAST_YEAR} DONE"
 
 
 time poetry run python prepare_run_features.py
 echo $(date -u +"%F %T") "prepare_run_features.py ${RACE_TYPE} ${FORECAST_YEAR} DONE"
-
-#time poetry run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=1200 --execute 2019-relay-simulation.ipynb
-#echo $(date -u +"%F %T") "2019-relay-simulation ${RACE_TYPE} ${FORECAST_YEAR} DONE"
 
 time poetry run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=1200 --execute relay-simulation-2024.ipynb
 echo $(date -u +"%F %T") "relay-simulation-2024.ipynb ${RACE_TYPE} ${FORECAST_YEAR} DONE"
