50070cbf63f2e27f8574a4837c25101863bb1f8e
Oskari Pirttikoski <oskari.pirttikoski@reaktor.fi>
2026-03-27 13:13:51 +0200

    first results from new version with leakage fixes

--- prepare_run_features.py
+++ prepare_run_features.py
@@ -85,7 +85,8 @@ def _load_ngboost_estimates_for_running_order_names():
 
 
     #results_dir = '~/koodi/jukola-ngboost/results/ngboost-dev'
-    results_dir = '~/koodi/jukola-ngboost/results/ngboost-student-t'
+    #results_dir = '~/koodi/jukola-ngboost/results/ngboost-student-t'
+    results_dir = '~/koodi/jukola-ngboost/results/ngboost-norm-tuned-reviewed'
     #results_dir = '~/koodi/jukola-ngboost/results/hgb_reg-dev'
     #results_dir = '~/koodi/jukola-ngboost/results/hgb-NO-BC-dev'
     forecasts_path = f'{results_dir}/running_order_samples_v2_{shared.race_id_str()}.json'
--- process-one-race.sh
+++ process-one-race.sh
@@ -11,7 +11,8 @@ echo $(date -u +"%F %T") "group_names ${RACE_TYPE} ${FORECAST_YEAR} DONE"
 
 echo $(date -u +"%F %T") "Starting ngboost  ${RACE_TYPE} ${FORECAST_YEAR} in another directory"
 # Run in subshell
-(cd ~/koodi/jukola-ngboost; time RACE_TYPE=${RACE_TYPE} FORECAST_YEAR=${FORECAST_YEAR} BATCH_RUN_TS=${RUN_TS} PROCESSING_BATCH_ID=ngboost-student-t uv run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=36000  --execute ngboost-norm-tuned.ipynb)
+#(cd ~/koodi/jukola-ngboost; time RACE_TYPE=${RACE_TYPE} FORECAST_YEAR=${FORECAST_YEAR} BATCH_RUN_TS=${RUN_TS} PROCESSING_BATCH_ID=ngboost-student-t uv run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=36000  --execute ngboost-norm-tuned.ipynb)
+(cd ~/koodi/jukola-ngboost; time RACE_TYPE=${RACE_TYPE} FORECAST_YEAR=${FORECAST_YEAR} BATCH_RUN_TS=${RUN_TS} PROCESSING_BATCH_ID=ngboost-norm-tuned-reviewed uv run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=36000  --execute ngboost-norm-tuned-reviewed.ipynb)
 #(cd ~/koodi/jukola-ngboost; time RACE_TYPE=${RACE_TYPE} FORECAST_YEAR=${FORECAST_YEAR} PROCESSING_BATCH_ID=ngboost-student-t uv run jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=36000  --execute ngboost-skwenorm.ipynb)
 echo $(date -u +"%F %T") "ngboost ${RACE_TYPE} ${FORECAST_YEAR} DONE"
 
