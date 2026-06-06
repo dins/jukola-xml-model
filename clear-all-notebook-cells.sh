207707008974b8c6f421a213f151cea0d78be6fd
Oskari Pirttikoski <oskari.pirttikoski@reaktor.fi>
2026-06-06 14:14:33 +0300

    update deps and fix some exploration notebooks

--- clear-all-notebook-cells.sh
+++ clear-all-notebook-cells.sh
@@ -3,7 +3,8 @@ set -eu -o pipefail
 
 # time ./clear-all-notebook-cells.sh
 
-for file in *.ipynb; do
+# Find all .ipynb files, excluding any hidden directories like .ipynb_checkpoints/ or .venv/
+find . -type f -name "*.ipynb" -not -path "*/\.*" | while read -r file; do
   echo $(date -u +"%F %T") "Clearing ${file}"
   uv run jupyter nbconvert --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True --inplace "${file}"
 done
